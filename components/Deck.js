fecd2f4e629bbd6bd4c9a02023a8cc25a268f0c4
Pnickolas1 <petertountas1@gmail.com>
2017-10-11 11:49:10 -0500

    small update to confirm functionality

---
+++ components/Deck.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Animated } from 'react-native';
+
+
+export default class Deck extends Component {
+    
+
+    render(){
+
+        return (
+            <TouchableOpacity>
+                <Text> This is a test </Text>
+            </TouchableOpacity>
+        )
+
+    }
+}
